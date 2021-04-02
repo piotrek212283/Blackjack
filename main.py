0fb35783d616aaaa2f31d41f1823b0e706669c7d
Piotrek <piotr.chrzanowski@>
2021-04-02 20:49:01 +0200

    Created player class

---
+++ main.py
@@ -0,0 +1,20 @@
+import deck
+import player
+
+
+def game():
+
+    board_deck = deck.Deck()
+    board_deck.shuffle()
+
+
+
+    while True:
+        a = 1
+
+
+
+
+
+if __name__ == '__main__':
+    game()
--- player.py
+++ player.py
@@ -1,3 +1,27 @@
-class Player:
+import pocket
+import card
 
-    def __init__(self, )
+
+class Player:
+    def __init__(self, name, balance):
+        self.name = name
+        self.player_pocket = pocket.Pocket(balance)
+        self.player_cards = []
+
+    def __str__(self):
+        return "Player %s have %d coins with total card sum of %d" % (
+            self.name,
+            self.player_pocket.balance,
+            self.cards_sum,
+        )
+
+    def take_card(self, cards):
+        if type(cards) == list:
+            self.player_cards.extend(cards)
+        else:
+            self.player_cards.append(cards)
+
+    def cards_sum(self):
+        sum = 0
+        for i in range(self.player_cards):
+            sum += self.player_cards[i].value
